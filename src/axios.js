6399f68257a54b235fade930508b69635216c5b6
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-23 00:28:59 +0400

    set api link

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "http://localhost:8800/",
+  baseURL: "https://movie-booking-api.vercel.app/",
   withCredentials: true,
 });
