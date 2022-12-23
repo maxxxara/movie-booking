c87e8fe4f5b09a60ed690a5f52f0e719eb545129
maxxxara <dati.maxarashvili24@gmail.com>
2022-12-23 11:37:36 +0400

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://movie-booking-fi77tcjlc-maxxxara.vercel.app/",
+  baseURL: "https://movie-booking-2yi20i7kn-maxxxara.vercel.app/",
   withCredentials: true,
 });
